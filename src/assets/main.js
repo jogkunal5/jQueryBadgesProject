bcd626af4217e72dc3c51a88ee6360ffa7ccf86e
jogkunal5 <jogkunal5@gmail.com>
2016-12-29 12:18:22 +0530

    Committed jquery changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
-
-  // your code will go here
-
+	$.ajax({
+    url: 'https://www.codeschool.com/users/jogkunal5.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var obj=response.courses.completed;
+      console.log(obj);
+      for (var key in obj) {
+    		console.log(key + " -> " + obj[key].title);
+    		$('#badges').append('<div class="course"><h3>'+obj[key].title+'</h3><img src="'+obj[key].badge+'"><a href="'+obj[key].url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+	  }
+	}
+  });
 });
